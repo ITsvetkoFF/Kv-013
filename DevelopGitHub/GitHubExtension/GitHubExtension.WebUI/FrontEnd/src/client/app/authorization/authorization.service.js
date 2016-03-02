faa1fd7df484f4f16090080e780f5288be237347
itsvetkoff <tsvetkoff@i.ua>
2016-03-02 17:14:56 +0200

    syntax check

--- DevelopGitHub/GitHubExtension/GitHubExtension.WebUI/FrontEnd/src/client/app/authorization/authorization.service.js
+++ DevelopGitHub/GitHubExtension/GitHubExtension.WebUI/FrontEnd/src/client/app/authorization/authorization.service.js
@@ -11,17 +11,16 @@
 
     // service responsible for authorization
     function Authorization($http) {
-		
-		// Base64 encoding service used by AuthenticationService
+        // Base64 encoding service used by AuthenticationService
         // Took from http://jasonwatmore.com/post/2015/03/10/AngularJS-User-Registration-and-Login-Example.aspx
         var Base64 = {
 
             keyStr: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=',
 
             encode: function (input) {
-                var output = "";
-                var chr1, chr2, chr3 = "";
-                var enc1, enc2, enc3, enc4 = "";
+                var output = '';
+                var chr1, chr2, chr3 = '';
+                var enc1, enc2, enc3, enc4 = '';
                 var i = 0;
 
                 do {
@@ -29,10 +28,10 @@
                     chr2 = input.charCodeAt(i++);
                     chr3 = input.charCodeAt(i++);
 
-                    enc1 = chr1 >> 2;
-                    enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-                    enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-                    enc4 = chr3 & 63;
+                    enc1 = chr1 >> 2; // jshint ignore:line
+                    enc2 = ((chr1 & 3) << 4) | (chr2 >> 4); // jshint ignore:line
+                    enc3 = ((chr2 & 15) << 2) | (chr3 >> 6); // jshint ignore:line
+                    enc4 = chr3 & 63; // jshint ignore:line
 
                     if (isNaN(chr2)) {
                         enc3 = enc4 = 64;
@@ -45,27 +44,27 @@
                         this.keyStr.charAt(enc2) +
                         this.keyStr.charAt(enc3) +
                         this.keyStr.charAt(enc4);
-                    chr1 = chr2 = chr3 = "";
-                    enc1 = enc2 = enc3 = enc4 = "";
+                    chr1 = chr2 = chr3 = '';
+                    enc1 = enc2 = enc3 = enc4 = '';
                 } while (i < input.length);
 
                 return output;
             },
 
             decode: function (input) {
-                var output = "";
-                var chr1, chr2, chr3 = "";
-                var enc1, enc2, enc3, enc4 = "";
+                var output = '';
+                var chr1, chr2, chr3 = '';
+                var enc1, enc2, enc3, enc4 = '';
                 var i = 0;
 
                 // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
                 var base64test = /[^A-Za-z0-9\+\/\=]/g;
                 if (base64test.exec(input)) {
-                    window.alert("There were invalid base64 characters in the input text.\n" +
-                        "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
-                        "Expect errors in decoding.");
+                    window.alert('There were invalid base64 characters in the input text.\n' +
+                        'Valid base64 characters are A-Z, a-z, 0-9, \'+\', \'/\',and \'=\'\n' +
+                        'Expect errors in decoding.');
                 }
-                input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+                input = input.replace(/[^A-Za-z0-9\+\/\=]/g, '');
 
                 do {
                     enc1 = this.keyStr.indexOf(input.charAt(i++));
@@ -73,39 +72,39 @@
                     enc3 = this.keyStr.indexOf(input.charAt(i++));
                     enc4 = this.keyStr.indexOf(input.charAt(i++));
 
-                    chr1 = (enc1 << 2) | (enc2 >> 4);
-                    chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-                    chr3 = ((enc3 & 3) << 6) | enc4;
+                    chr1 = (enc1 << 2) | (enc2 >> 4); // jshint ignore:line
+                    chr2 = ((enc2 & 15) << 4) | (enc3 >> 2); // jshint ignore:line
+                    chr3 = ((enc3 & 3) << 6) | enc4; // jshint ignore:line
 
                     output = output + String.fromCharCode(chr1);
 
-                    if (enc3 != 64) {
+                    if (enc3 !== 64) {
                         output = output + String.fromCharCode(chr2);
                     }
-                    if (enc4 != 64) {
+                    if (enc4 !== 64) {
                         output = output + String.fromCharCode(chr3);
                     }
 
-                    chr1 = chr2 = chr3 = "";
-                    enc1 = enc2 = enc3 = enc4 = "";
+                    chr1 = chr2 = chr3 = '';
+                    enc1 = enc2 = enc3 = enc4 = '';
 
                 } while (i < input.length);
 
                 return output;
             }
         };
-		
+
         return {
             signIn: signIn
         };
 
         function signIn(user) {
-			var credentials = Base64.encode(user.username + ":" + user.password);
-			
+            var credentials = Base64.encode(user.username + ':' + user.password);
+
             return $http({
                 method  : 'GET',
                 url     : 'https://api.github.com/user',
-                headers : { 'Authorization': 'Basic ' + credentials }
+                headers : {'Authorization': 'Basic ' + credentials}
             });
         }
     }
