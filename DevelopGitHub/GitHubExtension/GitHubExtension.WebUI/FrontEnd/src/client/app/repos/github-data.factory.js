93b8725887217b3cb4c3cc11e293d7dc12ca12de
Gelphy <a.zakrevskiy1@gmail.com>
2016-03-02 14:35:49 +0200

    Add HotTowel layout and repos list

---
+++ DevelopGitHub/GitHubExtension/GitHubExtension.WebUI/FrontEnd/src/client/app/repos/github-data.factory.js
@@ -0,0 +1,36 @@
+﻿(function () {
+    'use strict';
+    var module = angular.module('app.repos');
+
+    module.factory('githubData', githubData);
+
+    githubData.$inject = ['$http'];
+
+    /* @ngInject */
+    function githubData($http) {
+
+        var getUser = function (username) {
+
+            return $http.get("https://api.github.com/users/" + username)
+                        .then(function (response) {
+                            return response.data;
+                        });
+        };
+
+        var getRepos = function (user) {
+            return $http.get(user.repos_url)
+                  .then(function (response) {
+                      return response.data;
+                  });
+        }
+
+        return {
+            getUser: getUser,
+            getRepos: getRepos
+        };
+
+    };
+
+    
+
+}());
