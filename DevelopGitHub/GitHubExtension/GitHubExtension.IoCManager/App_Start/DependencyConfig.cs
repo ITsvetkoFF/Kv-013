68ec429063dae3f584a76c075b367438a2a0975e
Andrew Gumenyuk <dj_revo_1@mail.ru>
2016-03-06 02:36:58 +0000

    Add Simple Injector

---
+++ DevelopGitHub/GitHubExtension/GitHubExtension.IoCManager/App_Start/DependencyConfig.cs
@@ -0,0 +1,33 @@
+﻿using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using GitHubExtension.Domain;
+using GitHubExtension.Domain.Interfaces;
+using SimpleInjector;
+
+namespace GitHubExtension.IoCManager.App_Start
+{
+    [assembly: PreApplicationStartMethod(typeof(GitHubExtension.IoCManager.App_Start.DependencyConfig), "Initialize")]
+    public static class DependencyConfig
+    {
+        public static void Initialize()
+        {
+            var container = BuildContainer();
+
+            DependencyResolver.SetResolver(new SimpleInjectorDependencyResolver(container));
+            GlobalConfiguration.Configuration.DependencyResolver = new SimpleInjectorDependencyResolver(container);
+        }
+
+        public static Container BuildContainer()
+        {
+            var container = new Container();
+            var lifestyle = Lifestyle.Singleton;
+
+            container.Register<ISecurityDbContext, SecurityDbContext>(lifestyle);
+
+            container.Verify();
+
+            return container;
+        }
+    }
+}
