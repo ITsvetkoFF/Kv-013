b3430f8aac2f68f781366bd2cb0028444f979fd1
Vladyslav Romanchuk <senioroman4uk@gmail.com>
2016-03-13 21:30:56 +0200

    3 - add logic to API for GET (list of roles) with a route api/roles

---
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Security.WebApi/Converters/RoleConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using GitHubExtension.Security.DAL.Entities;
+using GitHubExtension.Security.WebApi.Models;
+using System.Linq;
+
+namespace GitHubExtension.Security.WebApi.Converters
+{
+    public static class RoleConverter
+    {
+        public static RoleViewModel ToRoleViewModel(this SecurityRole role)
+        {
+            return new RoleViewModel()
+            {
+                Id = role.Id,
+                Name = role.Name
+            };
+        }
+
+        public static IEnumerable<RoleViewModel> ToRoleViewModel(this IEnumerable<SecurityRole> securityRoles)
+        {
+            return securityRoles.Select(address => address.ToRoleViewModel());
+        }
+    }
+}
