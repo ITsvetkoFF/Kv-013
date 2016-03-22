743eec0152df00726f220b10a5b8842a5f76b330
Gelphy <a.zakrevskiy1@gmail.com>
2016-03-22 19:33:38 +0200

    Add localization service

--- DevelopGitHub/GitHubExtension/GitHubExtension.Security.WebApi/FrontEnd/src/client/app/layout/ht-top-nav.directive.js
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Security.WebApi/FrontEnd/src/client/app/layout/ht-top-nav.directive.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     'use strict';
 
     angular
@@ -6,7 +6,7 @@
         .directive('htTopNav', htTopNav);
 
     /* @ngInject */
-    function htTopNav () {
+    function htTopNav() {
         var directive = {
             bindToController: true,
             controller: TopNavController,
@@ -18,10 +18,14 @@
             templateUrl: 'app/layout/ht-top-nav.html'
         };
 
-        TopNavController.$inject = ['$state', 'routerHelper'];
+        TopNavController.$inject = ['i18n', '$state', 'routerHelper'];
         /* @ngInject */
-        function TopNavController($state, routerHelper) {
+        function TopNavController(i18n, $state, routerHelper) {
             var vm = this;
+
+            // add i18n for localization
+            vm.i18n = i18n;
+
             var states = routerHelper.getStates();
 
             activate();
@@ -31,14 +35,13 @@
             }
 
             function getNavRoutes() {
-                vm.navRoutes = states.filter(function(r) {
+                vm.navRoutes = states.filter(function (r) {
                     return r.settings && r.settings.topNav;
-                }).sort(function(r1, r2) {
+                }).sort(function (r1, r2) {
                     return r1.settings.topNav - r2.settings.topNav;
                 });
             }
         }
-
         return directive;
     }
 })();
---
+++ DevelopGitHub/GitHubExtension/GitHubExtension.WebUI/FrontEnd/src/client/app/blocks/localization/i18n.factory.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    'use strict';
+
+    angular
+      .module('blocks.localization')
+      .factory('i18n', i18nFactory);
+
+    i18nFactory.$inject = ['i18nMessages'];
+    function i18nFactory(i18nMessages) {
+        var service = {
+            setLanguage: setLanguage,
+            validLanguages: Object.keys(i18nMessages),
+        };
+        // Set default language as the first from the constants (En)
+        service.setLanguage(service.validLanguages[0]);
+        return service;
+
+        function setLanguage(newValue) {
+            service.currentLanguage = newValue;
+            service.message = i18nMessages[newValue];
+        }
+    }
+})();
