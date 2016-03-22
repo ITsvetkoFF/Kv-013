f6116f02307635cb904191cdfb17ffd09078d77d
Gelphy <a.zakrevskiy1@gmail.com>
2016-03-22 19:33:38 +0200

    Add localization service

--- DevelopGitHub/GitHubExtension/GitHubExtension.Security.WebApi/FrontEnd/src/client/app/admin/admin.controller.js
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Security.WebApi/FrontEnd/src/client/app/admin/admin.controller.js
@@ -5,10 +5,14 @@
         .module('app.admin')
         .controller('AdminController', AdminController);
 
-    AdminController.$inject = ['logger'];
+    AdminController.$inject = ['logger', 'i18n'];
     /* @ngInject */
-    function AdminController(logger) {
+    function AdminController(logger, i18n) {
         var vm = this;
+
+        // add localization
+        vm.i18n = i18n;
+
         vm.title = 'Admin';
 
         activate();
