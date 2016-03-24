673ed6c34330bba007060f7dfe083e87eaf6cbbc
Vladyslav Romanchuk <senioroman4uk@gmail.com>
2016-03-24 17:51:38 +0200

    Enable CORS SupportCredentials. Now we can be authorized even with CORS

--- DevelopGitHub/GitHubExtension/GitHubExtension.Security.WebApi.Library/Controllers/RolesController.cs
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Security.WebApi.Library/Controllers/RolesController.cs
@@ -1,11 +1,12 @@
 ﻿using System.Linq;
-using System.Web;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using GitHubExtension.Security.DAL.Interfaces;
 using GitHubExtension.Security.WebApi.Converters;
 
 namespace GitHubExtension.Security.WebApi.Library.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*", SupportsCredentials = true)]
     [Authorize(Roles = "Admin")]
     [RoutePrefix("api/roles")]
     public class RolesController : BaseApiController
