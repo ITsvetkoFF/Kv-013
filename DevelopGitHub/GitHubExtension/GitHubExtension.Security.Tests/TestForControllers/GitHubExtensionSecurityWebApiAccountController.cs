a806e949f145a0be5532a48431b8454a18d471dd
IrynaRobulets <irene9531q@gmail.com>
2016-03-25 18:18:59 +0200

    Test Account Controller GetUserById

---
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Security.Tests/TestForControllers/GitHubExtensionSecurityWebApiAccountController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using Xunit;
+using GitHubExtension.Security.DAL.Infrastructure;
+using Microsoft.AspNet.Identity;
+//using GitHubExtension.Security.StorageModels;
+using GitHubExtension.Security.StorageModels.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using GitHubExtension.Security.WebApi.Library.Converters;
+using GitHubExtension.Models.CommunicationModels;
+using GitHubExtension.Security.WebApi.Controllers;
+using GitHubExtension.Security.WebApi.Library.Services;
+using GitHubExtension.Security.DAL.Interfaces;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+
+
+namespace GitHubExtension.Security.Tests
+{
+    public class TestGitHubExtensionSecurityWebApiAccountController
+    {
+        [Fact]
+        public void GetUserByIdCheckStatusCodeIfNotFound()
+        {
+            //Arrange
+            string idToFind ="34";
+            User user = null;
+            var store = Substitute.For<IUserStore<User>>();
+            var userManager = Substitute.For<ApplicationUserManager>(store);
+            userManager.FindByIdAsync(idToFind).Returns(user);
+
+            AccountController controller = new AccountController(Substitute.For<IGithubService>(),
+                Substitute.For<ISecurityContext>(), Substitute.For<IAuthService>(), userManager, store,
+                Substitute.For<IRoleStore<IdentityRole, string>>());
+
+            //Act
+            IHttpActionResult NullUser = controller.GetUser(idToFind).Result;
+
+            //Assert
+            Assert.IsType<NotFoundResult>(NullUser);
+        }
+
+        [Fact]
+        public void GetUserByIdCheckStatusCodeIfUserFound()
+        {
+            string idToFind = "35";
+            User user = new User
+            {
+                ProviderId = 35,
+                GitHubUrl = "GitHubUrl"
+            };
+            var store = Substitute.For<IUserStore<User>>();
+            var userManager = Substitute.For<ApplicationUserManager>(store);
+            userManager.FindByIdAsync(idToFind).Returns(user);
+
+            AccountController controller = new AccountController(Substitute.For<IGithubService>(),
+                Substitute.For<ISecurityContext>(), Substitute.For<IAuthService>(), userManager, store,
+                Substitute.For<IRoleStore<IdentityRole, string>>());
+
+            IHttpActionResult result = controller.GetUser(idToFind).Result;
+            Assert.IsType<System.Web.Http.Results.OkNegotiatedContentResult<UserReturnModel>>(result);
+            //var identityUser = Substitute.For<>();
+            //identityUser//.ToUserReturnModel().//..ToUserReturnModel().
+            //Assert.Equels();
+        }
+        
+    }
+}
--- DevelopGitHub/GitHubExtension/GitHubExtension.Security.WebApi.Library/Controllers/AccountController.cs
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Security.WebApi.Library/Controllers/AccountController.cs
@@ -62,6 +62,7 @@ namespace GitHubExtension.Security.WebApi.Library.Controllers
             {
                 return Ok(user.ToUserReturnModel());
             }
+            
 
             return NotFound();
         }
