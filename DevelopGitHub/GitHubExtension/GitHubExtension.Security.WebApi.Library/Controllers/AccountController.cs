7496d26dd99d076de4fa511b861a6318e10c0f93
Vladyslav Romanchuk <senioroman4uk@gmail.com>
2016-03-25 18:43:58 +0200

    Fix saving claim to local Identity

--- DevelopGitHub/GitHubExtension/GitHubExtension.Security.WebApi.Library/Controllers/AccountController.cs
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Security.WebApi.Library/Controllers/AccountController.cs
@@ -99,7 +99,6 @@ namespace GitHubExtension.Security.WebApi.Library.Controllers
             if (repositoryRole != null)
                 appUser.UserRepositoryRoles.Remove(repositoryRole);
 
-
             appUser.UserRepositoryRoles.Add(new UserRepositoryRole()
             {
                 RepositoryId = repoId,
@@ -128,15 +127,6 @@ namespace GitHubExtension.Security.WebApi.Library.Controllers
             return Ok();
         }
         
-
-        // GET api/Account/ExternalLogin
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="provider"></param>
-        /// <param name="error"></param>
-        /// <returns></returns>
-
         // GET api/Account/ExternalLogin
         [OverrideAuthentication]
         [HostAuthentication(DefaultAuthenticationTypes.ExternalCookie)]
@@ -148,44 +138,60 @@ namespace GitHubExtension.Security.WebApi.Library.Controllers
                 return new ChallengeResult(provider, this);
 
             var claims = User.Identity as ClaimsIdentity;
-            string token = claims.FindFirstValue("ExternalAccessToken");
+            Claim tokenClaim = claims.FindFirst("ExternalAccessToken");
 
             //TODO: Log
-            if (token == null)
+            if (tokenClaim == null)
                 throw new TokenNotFoundException();
 
-            GitHubUserModel userReadModel = await _githubService.GetUserAsync(token);
+            GitHubUserModel userReadModel = await _githubService.GetUserAsync(tokenClaim.Value);
             User user = _userManager.FindByGitHubId(userReadModel.GitHubId);
 
             if (user == null)
             {
-                #region move to service
-                SecurityRole role = await _securityContext.SecurityRoles.FirstOrDefaultAsync(r => r.Name == "Admin");
-                if (role == null)
-                    return InternalServerError();
-                
-                GitHubUserModel gitHubUserModel = await _githubService.GetUserAsync(token);
-                List<RepositoryDto> repositories = await _githubService.GetReposAsync(token);
-                var repositoriesToAdd = repositories.Select(r => new UserRepositoryRole() { Repository = r.ToEntity(), SecurityRole = role }).ToList();
-                user = gitHubUserModel.ToUserEntity();
-                user.UserRepositoryRoles = repositoriesToAdd;
-                
-                IdentityResult addUserResult = await _userManager.CreateAsync(user);
-                if (!addUserResult.Succeeded)
-                    return GetErrorResult(addUserResult);
-
-                if (repositories.Any(r => !_userManager
-                        .AddClaim(user.Id,
-                            new Claim(role.Name, r.GitHubId.ToString())).Succeeded))
-                    return BadRequest();
-                #endregion
+                // if result is not null we have an error occured
+                IHttpActionResult registrationResult = await RegisterUser(tokenClaim.Value);
+                if (registrationResult != null)
+                    return registrationResult;
             }
 
-            HttpContext.Current.GetOwinContext().Authentication.SignIn(await user.GenerateUserIdentityAsync(_userManager, DefaultAuthenticationTypes.ApplicationCookie));
+            ClaimsIdentity localIdentity = await user.GenerateUserIdentityAsync(_userManager, DefaultAuthenticationTypes.ApplicationCookie);
+            localIdentity.AddClaim(tokenClaim);
+
+            var authentication = HttpContext.Current.GetOwinContext().Authentication;
+            authentication.SignOut(DefaultAuthenticationTypes.ExternalCookie);
+            authentication.SignIn(localIdentity);
+
             // TODO Need method for cookies in the future?
             HttpContext.Current.Response.Cookies["userName"].Value = user.UserName;
             HttpContext.Current.Response.Cookies["isAuth"].Value = "true";
+            
             return Redirect("http://localhost:3000/");
+        }
+
+        private async Task<IHttpActionResult> RegisterUser(string token)
+        {
+            SecurityRole role = await _securityContext.SecurityRoles.FirstOrDefaultAsync(r => r.Name == "Admin");
+            if (role == null)
+                return InternalServerError();
+
+            GitHubUserModel gitHubUserModel = await _githubService.GetUserAsync(token);
+            List<RepositoryDto> repositories = await _githubService.GetReposAsync(token);
+            
+            var repositoriesToAdd = repositories.Select(r => new UserRepositoryRole() { Repository = r.ToEntity(), SecurityRole = role }).ToList();
+            User user = gitHubUserModel.ToUserEntity();
+            user.UserRepositoryRoles = repositoriesToAdd;
+
+            IdentityResult addUserResult = await _userManager.CreateAsync(user);
+            if (!addUserResult.Succeeded)
+                return GetErrorResult(addUserResult);
+
+            if (repositories.Any(r => !_userManager
+                    .AddClaim(user.Id,
+                        new Claim(role.Name, r.GitHubId.ToString())).Succeeded))
+                return BadRequest();
+
+            return null;
         }
     }
 }
