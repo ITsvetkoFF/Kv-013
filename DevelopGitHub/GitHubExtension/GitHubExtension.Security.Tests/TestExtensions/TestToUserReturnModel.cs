556c8b5c70ac57704c52e53e8511d3b38e3562de
IrynaRobulets <irene9531q@gmail.com>
2016-03-26 20:12:14 +0200

    Add test TestToUserReturnModel

--- DevelopGitHub/GitHubExtension/GitHubExtension.Security.Tests/TestExtensions/TestToUserReturnModel.cs
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Security.Tests/TestExtensions/TestToUserReturnModel.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace GitHubExtension.Security.Tests.TestExtensions
 {
     class TestToUserReturnModel
     {
+        //public void 
     }
 }
