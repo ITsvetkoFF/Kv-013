c228ac03e585492a48048dfdea2b0a79acd414aa
Andrew Gumenyuk <dj_revo_1@mail.ru>
2016-03-26 11:34:12 +0000

    Add Configuration for tables in DB

---
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Security.DAL/Configuration/Tables/UserConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GitHubExtension.Security.StorageModels.Identity;
+
+namespace GitHubExtension.Security.DAL.Configuration.Tables
+{
+    public class UserConfiguration : EntityTypeConfiguration<User>
+    {
+        public string TableName
+        {
+            get { return "Users"; }
+        }
+        public UserConfiguration()
+        {
+            this.ToTable(TableName);
+        }
+    }
+}
