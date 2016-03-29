282afe6ff74345393ddcfb124ab0243f959cdb89
IrynaRobulets <irene9531q@gmail.com>
2016-03-30 02:19:00 +0300

    Refactoring AccountControllerTests

--- DevelopGitHub/GitHubExtension/GitHubExtension.Security.Tests/TestExtensions/TestUserMapper.cs
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Security.Tests/TestExtensions/TestUserMapper.cs
@@ -8,41 +8,41 @@ using NSubstitute;
 using GitHubExtension.Security.StorageModels.Identity;
 using GitHubExtension.Security.WebApi.Library.Mappers;
 using GitHubExtension.Models.CommunicationModels;
+using FluentAssertions;
 
 
 namespace GitHubExtension.Security.Tests.TestExtensions
 {
-    public class TestUserMapper
+    public class UserMapperTests
     {
         [Fact]
-        public void TestToUserReturnModel()
+        public void UserReturnModelTest()
         {
-            User user = new User
+            User EntityToUserReturnModelTest = new User
             {
                 Id = "123",
                 ProviderId = 124,
                 GitHubUrl = "GithubUrl",
                 UserName="UserName",
             };
-
             UserReturnModel expectedUserReturnModel = new UserReturnModel
             {
-                Id =user.Id,
-                Email = user.Email,
-                GitHubId = user.ProviderId,
-                UserName =  user.UserName
+                Id =EntityToUserReturnModelTest.Id,
+                Email = EntityToUserReturnModelTest.Email,
+                GitHubId = EntityToUserReturnModelTest.ProviderId,
+                UserName =  EntityToUserReturnModelTest.UserName
             };
             UserReturnModelComparer comparer =new UserReturnModelComparer();
 
-            UserReturnModel userReturnModel = user.ToUserReturnModel();
-
+            UserReturnModel userReturnModel = EntityToUserReturnModelTest.ToUserReturnModel();
+            
             Assert.Equal<UserReturnModel>(expectedUserReturnModel, userReturnModel, comparer);
+            
         }
     }
 
     public class UserReturnModelComparer : IEqualityComparer<UserReturnModel>
     {
-
         public bool Equals(UserReturnModel x, UserReturnModel y)
         {
             if (x.Id != y.Id) return false;
