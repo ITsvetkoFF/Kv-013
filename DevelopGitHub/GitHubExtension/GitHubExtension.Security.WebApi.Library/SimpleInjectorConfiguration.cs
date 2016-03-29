0679ba94973d6b93148e7ff7ed9ca29883aa06fb
Vladyslav Romanchuk <senioroman4uk@gmail.com>
2016-03-29 19:33:02 +0300

    Initialise project structure for activity

--- DevelopGitHub/GitHubExtension/GitHubExtension.Security.WebApi.Library/SimpleInjectorConfiguration.cs
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Security.WebApi.Library/SimpleInjectorConfiguration.cs
@@ -1,14 +1,5 @@
-﻿using System.Data.Entity;
-using System.Linq;
-using GitHubExtension.Security.DAL;
-using GitHubExtension.Security.DAL.Context;
-using GitHubExtension.Security.DAL.Infrastructure;
-using GitHubExtension.Security.DAL.Package;
-using GitHubExtension.Security.StorageModels.Identity;
+﻿using GitHubExtension.Security.DAL.Package;
 using GitHubExtension.Security.WebApi.Library.Package;
-using GitHubExtension.Security.WebApi.Library.Services;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
 using SimpleInjector;
 using SimpleInjector.Integration.WebApi;
 
