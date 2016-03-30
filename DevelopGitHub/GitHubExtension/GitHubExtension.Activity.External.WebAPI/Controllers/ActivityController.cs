b3259dfd7a045d0dd8f8f7b5344211f98276cbe1
Vladyslav Romanchuk <senioroman4uk@gmail.com>
2016-03-31 02:13:19 +0300

    redisign architecture start working on parsing events

--- DevelopGitHub/GitHubExtension/GitHubExtension.Activity.External.WebAPI/Controllers/ActivityController.cs
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Activity.External.WebAPI/Controllers/ActivityController.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http;
 
 namespace GitHubExtension.Activity.External.WebAPI
 {
-    public class Class1
+    public class ActivityController : ApiController
     {
+
+        public IHttpActionResult GetGitHubActivity()
+        {
+            
+        }
     }
 }
--- DevelopGitHub/GitHubExtension/GitHubExtension.Activity.External.WebAPI/Services/GitHubService.cs
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Activity.External.WebAPI/Services/GitHubService.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
-namespace GitHubExtension.Activity.External.Services
+namespace GitHubExtension.Activity.External.WebAPI.Services
 {
     public class GitHubService
     {
@@ -15,6 +16,26 @@ namespace GitHubExtension.Activity.External.Services
              //Need to set user-agent to access GitHub API, Using Chrome 48
             { "User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36" }
         };
+
+        public GitHubService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async void GetGitHubEvents(string owner, string repository, string token)
+        {
+            var message = CreateMessage(HttpMethod.Get, "");
+            HttpResponseMessage response = await _httpClient.SendAsync(message);
+            if (!response.IsSuccessStatusCode)
+                throw new Exception("Unsuccessful github request exception");
+
+            dynamic events = JArray.Parse(await response.Content.ReadAsStringAsync());
+            foreach (var @event in events)
+                Console.WriteLine(@event);
+
+            //JsonReader json = new JsonTextReader(new StreamReader(await response.Content.ReadAsStreamAsync()));
+
+        }
 
         private static HttpRequestMessage CreateMessage(HttpMethod method, string requestUri)
         {
