64c2ce8cc8277a4aa96f7ea6693556fffb2656de
IrynaRobulets <irene9531q@gmail.com>
2016-04-01 15:18:15 +0300

    Modify method UpdateProject

--- DevelopGitHub/GitHubExtension/GitHubExtension.Security.WebApi.Library/Controllers/UserController.cs
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Security.WebApi.Library/Controllers/UserController.cs
@@ -30,38 +30,38 @@ namespace GitHubExtension.Security.WebApi.Library.Controllers
             _userManager = userManager;
         }
 
-        [AllowAnonymous]
+
         [Route("api/user")]
         [HttpPatch]
         public async Task<IHttpActionResult> UpdateProject()
         {
             string request = await this.Request.Content.ReadAsStringAsync();
-            var repo = JsonConvert.DeserializeAnonymousType(request, new { repositoryId = "" });
+            var repo = JsonConvert.DeserializeAnonymousType(request, new { repository = "" });
             int repositoryId;
 
-            if(!Int32.TryParse(repo.repositoryId, out repositoryId))
+            if(!Int32.TryParse(repo.repository, out repositoryId))
             {
-                ModelState.AddModelError("repo", string.Format("repository with id '{0}' does not exists in the system", repo.repositoryId));
+                ModelState.AddModelError("repo", string.Format("repository with id '{0}' does not exists in the system", repo.repository));
                 return BadRequest(ModelState);
             }
 
-            
-            User user = await _userManager.Users.FirstOrDefaultAsync(x => x.Id == User.Identity.GetUserId());
+            string currentUserId = User.Identity.GetUserId();
+            User user = await _userManager.Users.FirstOrDefaultAsync(x => x.Id == currentUserId);
             if (user == null)
                 return NotFound();
             var repository = _securityContext.UserRepository.FirstOrDefault(r => r.RepositoryId == repositoryId && r.UserId == user.Id.ToString());
             if(repository == null)
             {
-                ModelState.AddModelError("repo", string.Format("user with id '{0}' does not have a repository with id '{1}'",user.Id, repo.repositoryId));
+                ModelState.AddModelError("repo", string.Format("user with id '{0}' does not have a repository with id '{1}'",user.Id, repo.repository));
                 return BadRequest(ModelState);
             }
 
             var claimsIdentity = await user.GenerateUserIdentityAsync(_userManager, DefaultAuthenticationTypes.ApplicationCookie);
-            var existingClaim = claimsIdentity.Claims.FirstOrDefault(c => c.Value == repositoryId.ToString());
+            var existingClaim = claimsIdentity.Claims.FirstOrDefault(c => c.Type == "CurrentProjectId");
             if (existingClaim != null)
                 _userManager.RemoveClaim(user.Id, existingClaim);
-            
-            var addClaimResult = await _userManager.AddClaimAsync(user.Id, new Claim("CurrentProject", repositoryId.ToString()));
+
+            var addClaimResult = await _userManager.AddClaimAsync(user.Id, new Claim("CurrentProjectId", repositoryId.ToString()));
 
             if (!addClaimResult.Succeeded)
             {
