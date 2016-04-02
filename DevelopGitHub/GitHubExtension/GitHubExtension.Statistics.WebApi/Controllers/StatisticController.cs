2c994b324ea426261a8b6f8c2490f8abe44376b1
Andrew Gumenyuk <dj_revo_1@mail.ru>
2016-04-02 01:15:12 +0100

    Add ability to get the commits from the github's repository

---
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Statistics.WebApi/Controllers/StatisticController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web.Http;
+using GitHubExtension.Statistics.WebApi.Models;
+using GitHubExtension.Statistics.WebApi.Services;
+using Microsoft.AspNet.Identity;
+
+namespace GitHubExtension.Statistics.WebApi.Controllers
+{
+    public class StatisticController : ApiController
+    {
+        private readonly IGitHubService _gitHubService;
+        public StatisticController(IGitHubService gitHubService)
+        {
+            this._gitHubService = gitHubService;
+        }
+
+        [Authorize]
+        [Route("api/user/commits")]
+        public async Task<IHttpActionResult> GetCommitsForUser()
+        {
+            var claims = User.Identity as ClaimsIdentity;
+            string token = claims.FindFirstValue("ExternalAccessToken");
+            string userName = User.Identity.GetUserName();
+
+            List<CommitDto> repositories = await _gitHubService.GetCommitsForUser(userName,"OnlineStrore.mArker",token);
+            
+            return Ok(repositories);
+        }
+    }
+}
---
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Statistics.WebApi/Models/CommitterDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace GitHubExtension.Statistics.WebApi.Models
+{
+    public class CommitterDto
+    {
+        [JsonProperty(PropertyName = "author")]
+        public GitHubUserModel Author { get; set; }
+
+        [JsonProperty(PropertyName = "message")]
+        public string Message { get; set; }
+    }
+}
