b873132b2f566371f3ecf8558c81a8a90a68067a
Andrew Gumenyuk <dj_revo_1@mail.ru>
2016-04-03 22:08:56 +0100

    Add opportunity get commits from our Web Api  and the graph display with these commits

---
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Security.WebApi/FrontEnd/src/client/app/stats/stats.factory.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    'use strict';
+    var module = angular.module('app.stats');
+
+    module.factory('statsFactory', statsFactory);
+
+    statsFactory.$inject = ['$http'];
+
+    function statsFactory($http) {
+        var baseUrl = 'api/user/commits';
+
+        function getCollaborators() {
+            return $http({
+                method: 'GET',
+                url: baseUrl
+            });
+        }
+
+        return {
+            getCollaborators: getCollaborators
+        };
+    }
+})();
