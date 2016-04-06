7644a9d6fd15561f35832ab6d74a8dd4f25c31ed
Vladyslav Romanchuk <senioroman4uk@gmail.com>
2016-04-06 10:05:51 +0300

    Add package configuration for external web-api

---
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Activity.External.WebAPI/Package/ExternalActivityPackage.cs
@@ -0,0 +1,14 @@
+﻿using GitHubExtension.Activity.External.WebAPI.Services;
+using SimpleInjector;
+using SimpleInjector.Packaging;
+
+namespace GitHubExtension.Activity.External.WebAPI.Package
+{
+    public class ExternalActivityPackage : IPackage
+    {
+        public void RegisterServices(Container container)
+        {
+            container.Register<IGitHubService, GitHubService>(Lifestyle.Scoped);
+        }
+    }
+}
