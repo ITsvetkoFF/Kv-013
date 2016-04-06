b061e4ef6c1c8f8ecaf54c67073d30facfdadd5f
Andrew Gumenyuk <dj_revo_1@mail.ru>
2016-04-06 21:03:01 +0100

    Add Pattern Builder and add RouteConstants

--- DevelopGitHub/GitHubExtension/GitHubExtension.Statistics.WebApi/CommunicationModels/Graph.cs
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Statistics.WebApi/CommunicationModels/Graph.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GitHubExtension.Statistics.WebApi.Models;
 
 namespace GitHubExtension.Statistics.WebApi.CommunicationModels
 {
@@ -13,13 +14,14 @@ namespace GitHubExtension.Statistics.WebApi.CommunicationModels
             this.UserInfo = new UserInfo();
             this.Commits = new List<int>();
             this.Months = new List<string>();
-            this.CommitsForEverRepository = new List<List<int>>();
-            this.RepositoriesName = new List<string>();
+            this.CommitsForEverRepository = new List<ICollection<int>>();
+            this.Repositories = new List<Repository>();
         }
+
         public UserInfo UserInfo { get; set; }
-        public List<int> Commits { get; set; }
-        public List<string> Months { get; set; }
-        public List<List<int>> CommitsForEverRepository { get; set; }
-        public List<string> RepositoriesName { get; set; } 
+        public virtual List<int> Commits { get; set; }
+        public virtual ICollection<string> Months { get; set; }
+        public virtual ICollection<ICollection<int>> CommitsForEverRepository { get; set; }
+        public virtual ICollection<Repository> Repositories { get; set; }
     }
 }
---
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Statistics.WebApi/Constant/RouteConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubExtension.Statistics.WebApi.Constant
+{
+    public class RouteConstants
+    {
+        public const string User = "api/user";
+        public const string GetUserCommits = User+"/"+"commits";
+        public const string GetRepoByName = GetUserCommits+"/"+"{name}";
+    }
+}
