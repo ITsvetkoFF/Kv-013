10b25f16e277f320f4662b7ba648679e4f7e130b
Vladyslav Romanchuk <senioroman4uk@gmail.com>
2016-04-06 10:08:20 +0300

    Finish with external activity controller

--- DevelopGitHub/GitHubExtension/GitHubExtension.Activity.External.WebAPI/Controllers/ActivityController.cs
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Activity.External.WebAPI/Controllers/ActivityController.cs
@@ -1,25 +1,44 @@
 ﻿using System.Threading.Tasks;
 using System.Web.Http;
 using GitHubExtension.Activity.External.WebAPI.Services;
+using Newtonsoft.Json.Linq;
 
 namespace GitHubExtension.Activity.External.WebAPI.Controllers
 {
     [Authorize]
     public class ActivityController : ApiController
     {
-        private GitHubService _gitHubService;
+        private IGitHubService _gitHubService;
 
-        public ActivityController()
+        public ActivityController(IGitHubService gitHubService)
         {
-            _gitHubService = new GitHubService();
+            _gitHubService = gitHubService;
         }
 
         [Route("api/activity/external")]
         public async Task<IHttpActionResult> GetGitHubActivity([FromUri] int page)
         {
-            var events = await _gitHubService.GetGitHubEventsAsync("ITsvetkoFF", "Kv-013", "7c021f975ee1e7e3209b9113d9346b7d5bb169f8");
-            
-            return Ok(events);
+            //This part is moved to extension method in another branch
+//            var claims = User.Identity as ClaimsIdentity;
+//            string token = claims.FindFirstValue("ExternalAccessToken");
+//            string currentProjectName = claims.FindFirstValue("CurrentProjectName");
+
+//            if (token == null)
+//                throw new TokenNotFoundException();
+
+//            string userName = User.Identity.GetUserName();
+
+//            var events = await _gitHubService.GetGitHubEventsAsync(userName, currentProjectName, token, page);
+            var events =
+                await _gitHubService.GetGitHubEventsAsync("ITsvetkoff", "Kv-013", "2660337e219b525b1c69cab89b0a0d13b93cb3bb", page);
+            int? numberOfPages = _gitHubService.GetNumberOfPages();
+            JObject response = new JObject();
+
+            response["events"] = new JArray(events);
+            if (numberOfPages.HasValue)
+                response["pages"] = numberOfPages;
+
+            return Ok(response);
         }
     }
 }
