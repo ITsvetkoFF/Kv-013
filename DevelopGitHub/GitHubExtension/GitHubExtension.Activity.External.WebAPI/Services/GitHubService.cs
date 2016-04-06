47fad9ff512bab08e85ddec0f83dffe5542d42a2
Vladyslav Romanchuk <senioroman4uk@gmail.com>
2016-04-06 10:01:05 +0300

    Finish github service for activity

--- DevelopGitHub/GitHubExtension/GitHubExtension.Activity.External.WebAPI/Services/GitHubService.cs
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Activity.External.WebAPI/Services/GitHubService.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using GitHubExtension.Activity.External.WebAPI.Exceptions;
 using Newtonsoft.Json.Linq;
 
 namespace GitHubExtension.Activity.External.WebAPI.Services
 {
-    public class GitHubService
+    public class GitHubService : IGitHubService
     {
+        private const string LastLinkRelRegex = "last";
+        private const string LinklRelPageRegex = @"page=(\d+)";
         private readonly HttpClient _httpClient;
         private IEnumerable<string> _lastRequestLinkHeader;
  
@@ -18,16 +22,16 @@ namespace GitHubExtension.Activity.External.WebAPI.Services
             { "User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36" }
         };
 
-        private const string EventsUrl = "https://api.github.com/repos/{0}/{1}/events";
+        private const string EventsUrl = "https://api.github.com/repos/{0}/{1}/events?access_token={2}&page={3}";
 
         public GitHubService()
         {
             _httpClient = new HttpClient();
         }
 
-        public async Task<IEnumerable<JToken>> GetGitHubEventsAsync(string owner, string repository, string token)
+        public async Task<IEnumerable<JToken>> GetGitHubEventsAsync(string owner, string repository, string token, int page)
         {
-            var message = CreateMessage(HttpMethod.Get, string.Format(EventsUrl, owner, repository));
+            var message = CreateMessage(HttpMethod.Get, string.Format(EventsUrl, owner, repository, token, page));
             HttpResponseMessage response = await _httpClient.SendAsync(message);
             if (!response.IsSuccessStatusCode)
                 throw new Exception("Unsuccessful github request exception");
@@ -36,7 +40,6 @@ namespace GitHubExtension.Activity.External.WebAPI.Services
             JArray events = JArray.Parse(await response.Content.ReadAsStringAsync());
 
             var parsedEvents = events.Children().Select(e => e.ParseEvent());
-            GetNumberOfPages();
 
             return parsedEvents;
         }
@@ -44,22 +47,34 @@ namespace GitHubExtension.Activity.External.WebAPI.Services
         /// <summary>
         /// Used for pagination, gets number of last page from link header of last request
         /// </summary>
-        /// <returns> int number of pages</returns>
+        /// <returns> int number of pages or null if we are on last page</returns>
         public int? GetNumberOfPages()
         {
             if (_lastRequestLinkHeader == null)
-                return -1;
+                throw new LinkHeaderMissingException();
 
-            var link = _lastRequestLinkHeader.FirstOrDefault();
+            string link = _lastRequestLinkHeader.FirstOrDefault();
             if (link == null)
-                return -2;
+                throw new LinkHeaderMissingException();
 
             string[][] parts = link.Split(',').Select(l => l.Split(';')).ToArray();
             if (parts.Any(p => p.Length != 2))
-                throw new Exception("parts of header can't be split by ;");
-            //parts.FirstOrDefault(p => p.)
+                throw new LinkHeaderFormatException();
+            string[] lastRel = parts.FirstOrDefault(p => Regex.IsMatch(p[1], LastLinkRelRegex));
 
-            return 0;
+            // last rel not found in header this means we are on the last page
+            if (lastRel == null)
+                return null;
+
+            Match lastPageMatch = Regex.Match(lastRel[0], LinklRelPageRegex);
+            if (!lastPageMatch.Success || lastPageMatch.Groups.Count != 2)
+                throw new LinkHeaderFormatException();
+
+            int lastPage;
+            if ( !int.TryParse(lastPageMatch.Groups[1].Value, out lastPage) )
+                throw new LinkHeaderFormatException();
+
+            return lastPage;
         }
 
         private static HttpRequestMessage CreateMessage(HttpMethod method, string requestUri)
---
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Activity.External.WebAPI/Services/IGitHubService.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace GitHubExtension.Activity.External.WebAPI.Services
+{
+    public interface IGitHubService
+    {
+        Task<IEnumerable<JToken>> GetGitHubEventsAsync(string owner, string repository, string token, int page);
+
+        /// <summary>
+        /// Used for pagination, gets number of last page from link header of last request
+        /// </summary>
+        /// <returns> int number of pages or null if we are on last page</returns>
+        int? GetNumberOfPages();
+    }
+}
