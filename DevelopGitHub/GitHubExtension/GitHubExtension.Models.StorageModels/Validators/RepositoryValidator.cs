aa88bbb6f8daa6938e7fc96d370e7fdd4f8bf388
SerhiiStepanovych <hunterla2@gmail.com>
2016-04-07 12:24:41 +0300

    Add validators for Repository and UserRepositoryRole

---
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Models.StorageModels/Validators/RepositoryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using GitHubExtension.Security.StorageModels.Identity;
+
+namespace GitHubExtension.Security.StorageModels.Validators
+{
+    public class RepositoryValidator : AbstractValidator<Repository>
+    {
+        public RepositoryValidator()
+        {
+            RuleFor(r => r.Id).NotEmpty().GreaterThanOrEqualTo(ValidationConstants.MinValueForId);
+        }
+    }
+}
---
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Models.StorageModels/Validators/ValidationConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace GitHubExtension.Security.StorageModels.Validators
+{
+    public static class ValidationConstants
+    {
+        public const int MinValueForId = 1;
+    }
+}
