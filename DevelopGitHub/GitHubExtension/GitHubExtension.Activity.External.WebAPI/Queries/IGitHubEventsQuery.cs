96d9d79ac1c23c37162df5c59d438f1017d4c4bd
Vladyslav Romanchuk <senioroman4uk@gmail.com>
2016-04-08 16:43:00 +0300

    Add interface for external activity query. Start implementing CQRS

---
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Activity.External.WebAPI/Queries/IGitHubEventsQuery.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using GitHubExtension.Activity.External.WebAPI.Models;
+
+namespace GitHubExtension.Activity.External.WebAPI.Queries
+{
+    interface IGitHubEventsQuery
+    {
+        Task<IEnumerable<GitHubEventModel>> GetGitHubEventsAsync(string fullRepositoryName, string token, int numberOfPage);
+        IEnumerable<string> RequestLinkHeader { get; }
+    }
+}
