97cd9f7bfc453af35be281b081559bdb5ee301ab
Vladyslav Romanchuk <senioroman4uk@gmail.com>
2016-04-09 14:31:16 +0300

    Implement GitHubEventsQuery

--- DevelopGitHub/GitHubExtension/GitHubExtension.Activity.External.WebAPI/Queries/IGitHubEventsQuery.cs
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Activity.External.WebAPI/Queries/IGitHubEventsQuery.cs
@@ -4,7 +4,7 @@ using GitHubExtension.Activity.External.WebAPI.Models;
 
 namespace GitHubExtension.Activity.External.WebAPI.Queries
 {
-    interface IGitHubEventsQuery
+    public interface IGitHubEventsQuery
     {
         Task<IEnumerable<GitHubEventModel>> GetGitHubEventsAsync(string fullRepositoryName, string token, int numberOfPage);
         IEnumerable<string> RequestLinkHeader { get; }
