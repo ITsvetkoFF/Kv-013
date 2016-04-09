4a4fd538c49a6f6e12ac11423bd0e160ca20ea80
Vladyslav Romanchuk <senioroman4uk@gmail.com>
2016-04-09 13:39:11 +0300

    Add general constants for GitHub

---
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Activity.External.WebAPI/GitHubConstants.cs
@@ -0,0 +1,39 @@
+﻿namespace GitHubExtension.Activity.External.WebAPI
+{
+    public static class GitHubConstants
+    {
+        public const string UserAgentHeader = "User-Agent";
+        public const string UserAgentHeaderValue = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36";
+
+        public const string RefType = "ref_type";
+        public const string MasterBranch = "master_branch";
+        public const string DefaultBranch = "default_branch";
+        public const string PusherType = "pusher_type";
+        public const string CreatedAt = "created_at";
+        public const string UpdatedAt = "updated_at";
+        public const string PushedAt = "pushed_at";
+        public const string PublishedAt = "published_at";
+        public const string HtmlUrl = "html_url";
+        public const string UploadUrl = "upload_url";
+        public const string SiteAdmin = "site_admin";
+        public const string AvatarUrl = "avatar_url";
+        public const string GravatarId = "gravatar_id";
+        public const string CommitId = "commit_id";
+        public const string FullName = "full_name";
+        public const string IssueUrl = "issue_url";
+        public const string TagName = "tag_name";
+        public const string TargetCommitish = "target_commitish";
+        public const string DownloadCount = "download_count";
+        public const string ContentType = "content_type";
+        public const string PullRequest = "pull_request";
+        public const string ClosedAt = "closed_at";
+        public const string MergedAt = "merged_at";
+        public const string MergeCommitSha = "merge_commit_sha";
+        public const string MergeableState = "mergeable_state";
+        public const string MergedBy = "merged_by";
+        public const string ReviewComments = "review_comments";
+        public const string ChangedFiles = "changed_files";
+        public const string DistinctSize = "distinct_size";
+        public const string PageName = "page_name";
+    }
+}
