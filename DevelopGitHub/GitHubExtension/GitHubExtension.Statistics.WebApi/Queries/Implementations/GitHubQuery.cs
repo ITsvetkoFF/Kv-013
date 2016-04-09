15749dac57b8a0fda7d73f77664f008bfcac3b0a
Andrew Gumenyuk <dj_revo_1@mail.ru>
2016-04-09 15:00:08 +0100

    Add GitHubQuery

---
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Statistics.WebApi/Queries/Implementations/GitHubQuery.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using GitHubExtension.Statistics.WebApi.CommunicationModels;
+using GitHubExtension.Statistics.WebApi.Mappers.HttpRequest;
+using GitHubExtension.Statistics.WebApi.Queries.Constant;
+using GitHubExtension.Statistics.WebApi.Queries.Interfaces;
+using Newtonsoft.Json;
+
+namespace GitHubExtension.Statistics.WebApi.Queries.Implementations
+{
+    public class GitHubQuery : IGitHubQuery
+    {
+        private readonly HttpClient _httpClient;
+
+        public GitHubQuery()
+        {
+            this._httpClient = new HttpClient();
+        }
+
+        public async Task<List<int>> UserCommitsInYear(string owner, string repository, string token)
+        {
+            var requestUri = string.Format(GitHubQueryConstant.GetRepositoryCommits, owner, repository, token);
+            var response = await _httpClient.SendAsync(CreateMessageMapper.CreateMessage(HttpMethod.Get, requestUri));
+            RepositoryCommitsModel commitsFromRepository = JsonConvert.DeserializeObject<RepositoryCommitsModel>(await response.Content.ReadAsStringAsync());
+            return commitsFromRepository.CommitsOwner;
+        }
+
+        public async Task<List<RepositoryModel>> GetRepositories(string owner, string token)
+        {
+            var requestUri = string.Format(GitHubQueryConstant.GetRepositories, owner, token);
+            var response = await _httpClient.SendAsync(CreateMessageMapper.CreateMessage(HttpMethod.Get, requestUri));
+            List<RepositoryModel> repositories =
+                JsonConvert.DeserializeObject<List<RepositoryModel>>(await response.Content.ReadAsStringAsync());
+            return repositories;
+        }
+
+        public async Task<int> GetFollowersCount(string owner, string token)
+        {
+            var requestUri = string.Format(GitHubQueryConstant.GetFollowers, owner, token);
+            var response = await _httpClient.SendAsync(CreateMessageMapper.CreateMessage(HttpMethod.Get, requestUri));
+            List<GitHubUserModel> followers =
+                JsonConvert.DeserializeObject<List<GitHubUserModel>>(await response.Content.ReadAsStringAsync());
+            return followers.Count;
+        }
+
+        public async Task<int> GetFolowingCount(string owner, string token)
+        {
+            var requestUri = string.Format(GitHubQueryConstant.GetFollowing, owner, token);
+            var response = await _httpClient.SendAsync(CreateMessageMapper.CreateMessage(HttpMethod.Get, requestUri));
+            List<GitHubUserModel> listOfFolowing =
+                JsonConvert.DeserializeObject<List<GitHubUserModel>>(await response.Content.ReadAsStringAsync());
+            return listOfFolowing.Count;
+        }
+    }
+}
---
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Statistics.WebApi/Queries/Interfaces/IGitHubQuery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using GitHubExtension.Statistics.WebApi.CommunicationModels;
+
+namespace GitHubExtension.Statistics.WebApi.Queries.Interfaces
+{
+    public interface IGitHubQuery
+    {
+        Task<List<int>> UserCommitsInYear(string owner, string repository, string token);
+        Task<List<RepositoryModel>> GetRepositories(string owner, string token);
+        Task<int> GetFollowersCount(string owner, string token);
+        Task<int> GetFolowingCount(string owner, string token);
+    }
+}
