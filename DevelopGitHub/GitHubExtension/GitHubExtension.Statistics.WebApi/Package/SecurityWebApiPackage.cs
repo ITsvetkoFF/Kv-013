6b8089e00165c08922c781a44c77f2810f2546d6
Andrew Gumenyuk <dj_revo_1@mail.ru>
2016-04-13 21:48:31 +0100

    Delete GraphBLL from Package's Simple Injector

--- DevelopGitHub/GitHubExtension/GitHubExtension.Statistics.WebApi/Package/SecurityWebApiPackage.cs
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Statistics.WebApi/Package/SecurityWebApiPackage.cs
@@ -1,6 +1,4 @@
-﻿using GitHubExtension.Statistics.WebApi.BLL.Implementations;
-using GitHubExtension.Statistics.WebApi.BLL.Interfaces;
-using GitHubExtension.Statistics.WebApi.Queries.Implementations;
+﻿using GitHubExtension.Statistics.WebApi.Queries.Implementations;
 using GitHubExtension.Statistics.WebApi.Queries.Interfaces;
 using SimpleInjector;
 using SimpleInjector.Packaging;
@@ -12,7 +10,6 @@ namespace GitHubExtension.Statistics.WebApi.Package
         public void RegisterServices(Container container)
         {
             container.Register<IStatisticsQuery, StatisticsQuery>(Lifestyle.Transient);
-            container.Register<IGraphBll, GraphsBll>(Lifestyle.Transient);
             container.Register<IGitHubQuery, GitHubQuery>(Lifestyle.Transient);
         }
     }
