8ba289134e4aa5a6ec68b971e401bec2b00026bb
Andrew Gumenyuk <dj_revo_1@mail.ru>
2016-04-13 21:47:00 +0100

    Add constants to StatisticsRouteConstants

--- DevelopGitHub/GitHubExtension/GitHubExtension.Statistics.WebApi/Constant/StatisticsRouteConstants.cs
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Statistics.WebApi/Constant/StatisticsRouteConstants.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GitHubExtension.Statistics.WebApi.Constant
+﻿namespace GitHubExtension.Statistics.WebApi.Constant
 {
     public static class StatisticsRouteConstants
     {
-        public const string User = "api/user";
+        public const string User = "api/user/stats";
         public const string GetUserCommits = User + "/" + "commits";
         public const string GetRepoByName = GetUserCommits + "/" + "{name}";
+        public const string UserInfo = User + "/info";
+        public const string GetFollowers = UserInfo + "/followers";
+        public const string GetFollowing = UserInfo + "/following";
+        public const string GetRepositoriesCount = UserInfo + "/repositories-count";
+        public const string GetActivityMonths = User + "/activity-months";
+        public const string GetRepositories = User + "/repositories";
+        public const string GetCommitsRepositories = User + "/repositories/commits";
+        public const string GetGroupCommits = User + "/repositories/group-commits";
     }
 }
