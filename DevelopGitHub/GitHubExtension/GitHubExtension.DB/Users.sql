2c48f08ed6bae5baa435533b18b0e3615819d051
Rudeka <thotubox@gmail.com>
2016-04-13 19:39:40 +0300

    Add Notes and Users tables in a root DB project

---
+++ DevelopGitHub/GitHubExtension/GitHubExtension.DB/Users.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]                   NVARCHAR (128) NOT NULL,
+    [ProviderId]           INT            NOT NULL,
+    [GitHubUrl]            NVARCHAR (MAX) NULL,
+    [Email]                NVARCHAR (MAX) NULL,
+    [EmailConfirmed]       BIT            NOT NULL,
+    [PasswordHash]         NVARCHAR (MAX) NULL,
+    [SecurityStamp]        NVARCHAR (MAX) NULL,
+    [PhoneNumber]          NVARCHAR (MAX) NULL,
+    [PhoneNumberConfirmed] BIT            NOT NULL,
+    [TwoFactorEnabled]     BIT            NOT NULL,
+    [LockoutEndDateUtc]    DATETIME       NULL,
+    [LockoutEnabled]       BIT            NOT NULL,
+    [AccessFailedCount]    INT            NOT NULL,
+    [UserName]             NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_dbo.Users] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
