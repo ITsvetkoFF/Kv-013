3f56f48619a3c538fe65dfe44c9d1f9b9c9b1e6f
Rudeka <thotubox@gmail.com>
2016-04-13 19:46:24 +0300

    Add Notes DAL and connection string to EntryPoint

---
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Notes.DAL/Package/NotesDALPackage.cs
@@ -0,0 +1,14 @@
+﻿using GitHubExtension.Notes.DAL.Model;
+using SimpleInjector;
+using SimpleInjector.Packaging;
+
+namespace GitHubExtension.Notes.DAL.Package
+{
+    public class NotesDALPackage : IPackage
+    {
+        public void RegisterServices(Container container)
+        {
+            container.Register<NoteContext>(Lifestyle.Scoped);
+        }
+    }
+}
