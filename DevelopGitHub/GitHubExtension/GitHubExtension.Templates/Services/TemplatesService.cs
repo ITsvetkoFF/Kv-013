3a16c2e6e148d2c27cbb92298408ae178f78c6b7
alexrrrr <robinzon.alex@gmail.com>
2016-04-17 11:47:09 +0300

    Extract CreateMessage() to extension method.

--- DevelopGitHub/GitHubExtension/GitHubExtension.Templates/Services/TemplatesService.cs
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Templates/Services/TemplatesService.cs
@@ -10,17 +10,6 @@ namespace GitHubExtension.Templates.Services
         private readonly HttpClient _httpClient;
         private const string RouteToRepository = "https://api.github.com/repos/";
         private const string Contents = "/contents/";
-        private const string UserAgent = "User-Agent";
-        private const string UserAgentContent =
-            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36";
-        private static readonly Dictionary<string, string> DefaultHeaders = new Dictionary<string, string>
-
-        {
-            //Need to set user-agent to access GitHub API, Using Chrome 48
-            {
-                UserAgent,UserAgentContent
-            }
-        };
 
         public TemplatesService()
         {
@@ -31,7 +20,7 @@ namespace GitHubExtension.Templates.Services
             string pathToFile)
         {
             var pullRequestTemplateUri = GetTemplatesRequestUri(userName, repositoryName, pathToFile);
-            var message = CreateMessage(HttpMethod.Get, pullRequestTemplateUri);
+            var message = new HttpRequestMessage(HttpMethod.Get, pullRequestTemplateUri).CreateMessage();
             var response = await _httpClient.GetResponse(message);
 
             return response;
@@ -40,20 +29,10 @@ namespace GitHubExtension.Templates.Services
         public async Task<HttpResponseMessage> GetIssueTemplateAsync(string userName, string repositoryName, string pathToFile)
         {
             var issueTemplateUri = GetTemplatesRequestUri(userName, repositoryName, pathToFile);
-            var message = CreateMessage(HttpMethod.Get, issueTemplateUri);
+            var message = new HttpRequestMessage(HttpMethod.Get, issueTemplateUri).CreateMessage();
             var response = await _httpClient.GetResponse(message);
             
             return response;
-        }
-
-        private static HttpRequestMessage CreateMessage(HttpMethod method, string requestUri)
-        {
-            var message = new HttpRequestMessage(method, requestUri);
-            foreach (var header in DefaultHeaders)
-            {
-                message.Headers.Add(header.Key, header.Value);
-            }
-            return message;
         }
 
         private static string GetTemplatesRequestUri(string userName, string repositoryName, string pathToFile)
