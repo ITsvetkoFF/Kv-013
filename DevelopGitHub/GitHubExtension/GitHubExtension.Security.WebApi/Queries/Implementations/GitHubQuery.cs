9abe3dfc714dbb70db80722e86a9a6900aa14cce
Andrew Gumenyuk <dj_revo_1@mail.ru>
2016-04-20 17:48:56 +0100

    Add GitHubQuery and Constants

---
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Security.WebApi/Queries/Implementations/GitHubQuery.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using GitHubExtension.Security.WebApi.Exceptions;
+using GitHubExtension.Security.WebApi.Extensions.HttpRequestMessage;
+using GitHubExtension.Security.WebApi.Models;
+using GitHubExtension.Security.WebApi.Queries.Constant;
+using GitHubExtension.Security.WebApi.Queries.Interfaces;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace GitHubExtension.Security.WebApi.Queries.Implementations
+{
+    public class GitHubQuery : IGitHubQuery
+    {
+        private readonly HttpClient _httpClient;
+
+        public GitHubQuery()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<GitHubUserModel> GetUserAsync(string token)
+        {
+            var requestUri = string.Format(GitHubQueryConstant.GetUser, token);
+            var message = HttpRequestMessageExtension.CreateMessage(HttpMethod.Get, requestUri);
+
+            var response = await _httpClient.SendAsync(message);
+            if (!response.IsSuccessStatusCode)
+                throw new UnsuccessfullGitHubRequestException();
+
+            var dto = JsonConvert.DeserializeObject<GitHubUserModel>(await response.Content.ReadAsStringAsync());
+            dto.Email = dto.Email ?? await GetPrimaryEmailForUser(token);
+
+            return dto;
+        }
+
+        public async Task<string> GetPrimaryEmailForUser(string token)
+        {
+            var requestUri = string.Format(GitHubQueryConstant.GetUserEmail, token);
+            var message = HttpRequestMessageExtension.CreateMessage(HttpMethod.Get, requestUri);
+
+            var response = await _httpClient.SendAsync(message);
+
+            if (!response.IsSuccessStatusCode)
+                throw new UnsuccessfullGitHubRequestException();
+
+            var emails = JArray.Parse(await response.Content.ReadAsStringAsync());
+            var email = "";
+
+            foreach (var typedEntry in emails.Children()
+                .Select(emailEntry => JsonConvert.DeserializeAnonymousType
+                    (emailEntry.ToString(), new { Email = "", Primary = false }))
+                .Where(typedEntry => typedEntry.Primary))
+            {
+                email = typedEntry.Email;
+                break;
+            }
+            return email;
+        }
+
+        public async Task<List<CollaboratorModel>> GetCollaboratorsForRepo(string owner, string repository, string token)
+        {
+            var requestUri = string.Format(GitHubQueryConstant.GetCollaboratorsRepo, owner, repository, token);
+            var message = HttpRequestMessageExtension.CreateMessage(HttpMethod.Get, requestUri);
+
+            var response = await _httpClient.SendAsync(message);
+
+            if (!response.IsSuccessStatusCode)
+                throw new UnsuccessfullGitHubRequestException();
+
+            List<CollaboratorModel> collaboratorModels =
+                JsonConvert.DeserializeObject<List<CollaboratorModel>>(await response.Content.ReadAsStringAsync());
+            return collaboratorModels;
+        }
+
+        public async Task<List<RepositoryViewModel>> GetReposAsync(string token)
+        {
+            //Geting repos for user
+            var requestUri = string.Format(GitHubQueryConstant.GetRepos, token);
+            var message = HttpRequestMessageExtension.CreateMessage(HttpMethod.Get, requestUri);
+
+            var response = await _httpClient.SendAsync(message);
+            if (!response.IsSuccessStatusCode)
+                throw new UnsuccessfullGitHubRequestException();
+
+            List<RepositoryViewModel> collaboratorModels =
+                JsonConvert.DeserializeObject<List<RepositoryViewModel>>(await response.Content.ReadAsStringAsync());
+            return collaboratorModels;
+        }
+    }
+}
---
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Security.WebApi/Queries/Interfaces/IGitHubQuery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GitHubExtension.Security.WebApi.Models;
+
+namespace GitHubExtension.Security.WebApi.Queries.Interfaces
+{
+    public interface IGitHubQuery
+    {
+        Task<GitHubUserModel> GetUserAsync(string token);
+        Task<string> GetPrimaryEmailForUser(string token);
+        Task<List<CollaboratorModel>> GetCollaboratorsForRepo(string owner, string repository, string token);
+        Task<List<RepositoryViewModel>> GetReposAsync(string token);
+    }
+}
