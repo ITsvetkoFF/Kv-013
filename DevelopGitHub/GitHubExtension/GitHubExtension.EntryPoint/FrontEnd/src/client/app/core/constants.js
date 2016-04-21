d00f292510edb840bddc7f6b5e5b9b32c49f769b
Andrew Gumenyuk <dj_revo_1@mail.ru>
2016-04-21 01:30:59 +0100

    Add statistics' constants to constant.js

--- DevelopGitHub/GitHubExtension/GitHubExtension.EntryPoint/FrontEnd/src/client/app/core/constants.js
+++ DevelopGitHub/GitHubExtension/GitHubExtension.EntryPoint/FrontEnd/src/client/app/core/constants.js
@@ -8,6 +8,8 @@
         .constant('moment', moment)
         .constant('API_URL', (function () {
             var resource = 'http://localhost:50859/api/';
+            var baseUserStats = resource + 'user/stats';
+            var baseUserStatsInfo = baseUserStats + '/info';
             return {
                 BASE_URL: resource,
                 LOGIN: resource + 'account/ExternalLogin?provider=GitHub',
@@ -15,6 +17,15 @@
                 internalActivityUrl: resource + 'activity/internal',
                 externalActivityUrl: resource + 'activity/external?page=',
                 UPLOADPHOTO: resource + 'account/avatar'
+
+                GET_REPOBYNAME: baseUserStats + '/commits',
+                GET_FOLLOWERS: baseUserStatsInfo + '/followers',
+                GET_FOLLOWING: baseUserStatsInfo + '/following',
+                GET_REPOSITORIESCOUNT: baseUserStatsInfo + '/repositories-count',
+                GET_ACTIVITYMONTHS: baseUserStats + '/activity-months',
+                GET_REPOSITORIES: baseUserStats + '/repositories',
+                GET_COMMITSREPOSITORIES: baseUserStats + '/repositories/commits',
+                GET_GROUPCOMMITS: baseUserStats + 'repositories/group-commits'
             };
         })());
 })();
