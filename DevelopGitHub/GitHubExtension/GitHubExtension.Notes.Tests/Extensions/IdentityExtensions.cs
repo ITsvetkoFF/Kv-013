fec3f10c83721357d9144cd84af984a927a47eff
Rudeka <thotubox@gmail.com>
2016-04-25 18:22:08 +0300

    Add tests for CreateNote method

---
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Notes.Tests/Extensions/IdentityExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Claims;
+using System.Security.Principal;
+using System.Web.Http;
+using NSubstitute;
+
+namespace GitHubExtension.Notes.Tests.Extensions
+{
+    public static class IdentityExtensions
+    {
+        public static void SetUserForController(this ApiController controller, string name, string typeOfClaim, string valueOfClaim)
+        {
+            var identity = Substitute.ForPartsOf<GenericIdentity>(name);
+            identity.AddClaim(new Claim(typeOfClaim, valueOfClaim));
+            var principal = Substitute.ForPartsOf<GenericPrincipal>(identity, new[] { "user" });
+            controller.User = principal;
+        }
+    }
+}
