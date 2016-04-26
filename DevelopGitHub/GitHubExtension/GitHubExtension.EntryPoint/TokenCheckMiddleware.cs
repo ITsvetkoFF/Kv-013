9b7ba1e24f135f4320976d9e086c3ba599f14a9c
Vladyslav Romanchuk <senioroman4uk@gmail.com>
2016-04-26 16:38:40 +0300

    Add middleware for token check

---
+++ DevelopGitHub/GitHubExtension/GitHubExtension.EntryPoint/TokenCheckMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+
+using GitHubExtension.Infrastructure.Constants;
+using GitHubExtension.Infrastructure.Extensions.Owin;
+
+using Microsoft.Owin;
+
+namespace GitHubExtension.EntryPoint
+{
+    /// <summary>
+    /// Middleware that check external access token presence in claims
+    /// </summary>
+    public class TokenCheckMiddleware : OwinMiddleware
+    {
+        private const string NoToken = "You don't have access token to enter this resource";
+
+        public TokenCheckMiddleware(OwinMiddleware next)
+            : base(next)
+        {
+        }
+
+        public override async Task Invoke(IOwinContext context)
+        {
+            if (context.IsAuthenticated())
+            {
+                if (!context.Authentication.User.HasClaim(c => c.Type == "ExternalAccessToken"))
+                {
+                    context.Authentication.SignOut();
+                    context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                    await context.Response.WriteAsync(NoToken);
+                }
+            }
+
+            await Next.Invoke(context);
+        }
+    }
+}
---
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Infrastructure.Extensions/Owin/AuthenticationExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+
+namespace GitHubExtension.Infrastructure.Extensions.Owin
+{
+    public static class AuthenticationExtension
+    {
+        public static bool IsAuthenticated(this IOwinContext context)
+        {
+            return context.Authentication.User != null && context.Authentication.User.Identity != null
+                   && context.Authentication.User.Identity.IsAuthenticated;
+        }
+    }
+}
