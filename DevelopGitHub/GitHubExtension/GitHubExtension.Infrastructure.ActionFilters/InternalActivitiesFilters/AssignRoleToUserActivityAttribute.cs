3d37f36490f7bfb45c5980ccfa631d1de953f5f9
Gelphy <a.zakrevskiy1@gmail.com>
2016-05-11 20:08:10 +0300

    Add DI to attribute constractor, fix assign role attribute

--- DevelopGitHub/GitHubExtension/GitHubExtension.Infrastructure.ActionFilters/InternalActivitiesFilters/AssignRoleToUserActivityAttribute.cs
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Infrastructure.ActionFilters/InternalActivitiesFilters/AssignRoleToUserActivityAttribute.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Web;
 using System.Web.Http.Filters;
 using GitHubExtension.Activity.DAL;
 using GitHubExtension.Activity.Internal.WebApi.Commands;
@@ -8,36 +7,37 @@ using GitHubExtension.Activity.Internal.WebApi.Queries;
 using GitHubExtension.Security.DAL.Identity;
 using GitHubExtension.Security.DAL.Infrastructure;
 using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
 
 namespace GitHubExtension.Infrastructure.ActionFilters.InternalActivitiesFilters
 {
     [AttributeUsage(AttributeTargets.Method)]
-    public class AssignRoleToUserActivityAttribute : ActionFilterAttribute
+    public class AssignRoleToUserActivityAttribute : InternalActivityFilter
     {
+        public AssignRoleToUserActivityAttribute(
+                                                IActivityContextQuery activityContextQuery, 
+                                                IActivityContextCommand activityContextCommand, 
+                                                ApplicationUserManager applicationUserManager)
+            : base(activityContextQuery, activityContextCommand, applicationUserManager)
+        {
+        }
+
         public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
         {
             int repoId = (int)actionExecutedContext.ActionContext.ActionArguments["repoId"];
             string roleToAssign = (string)actionExecutedContext.ActionContext.ActionArguments["roleToAssign"];
             int gitHubId = (int)actionExecutedContext.ActionContext.ActionArguments["gitHubId"];
 
-            var userId = HttpContext.Current.User.Identity.GetUserId();
-            var userName = HttpContext.Current.User.Identity.Name;
+            var userId = actionExecutedContext.ActionContext.RequestContext.Principal.Identity.GetUserId();
+            var userName = actionExecutedContext.ActionContext.RequestContext.Principal.Identity.Name;
 
-            var contextQuery = HttpContext.Current.GetOwinContext().Get<ActivityContextQuery>();
-
-            var contextCommand = HttpContext.Current.GetOwinContext().Get<ActivityContextCommand>();
-
-            var userManager = HttpContext.Current.GetOwinContext().Get<ApplicationUserManager>();
-
-            User appUser = userManager.FindByGitHubId(gitHubId);
+            User appUser = _applicationUserManager.FindByGitHubId(gitHubId);
 
             string collaboratorName = appUser.UserName;
 
-            var activityType = contextQuery.GetUserActivityType(ActivityTypeNames.AddRole);
+            var activityType = _activityContextQuery.GetUserActivityType(ActivityTypeNames.AddRole);
 
-            contextCommand.AddActivity(new ActivityEvent()
-            { 
+            _activityContextCommand.AddActivity(new ActivityEvent()
+            {
                 UserId = userId,
                 ActivityTypeId = activityType.Id,
                 CurrentRepositoryId = repoId,
@@ -51,5 +51,7 @@ namespace GitHubExtension.Infrastructure.ActionFilters.InternalActivitiesFilters
 
             });
         }
+
+   
     }
 }
---
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Infrastructure.ActionFilters/InternalActivitiesFilters/InternalActivityFilter.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http.Filters;
+using GitHubExtension.Activity.Internal.WebApi.Commands;
+using GitHubExtension.Activity.Internal.WebApi.Queries;
+using GitHubExtension.Security.DAL.Infrastructure;
+
+namespace GitHubExtension.Infrastructure.ActionFilters.InternalActivitiesFilters
+{
+    public abstract class InternalActivityFilter : ActionFilterAttribute
+    {
+        protected readonly IActivityContextQuery _activityContextQuery;
+        protected readonly IActivityContextCommand _activityContextCommand;
+        protected readonly ApplicationUserManager _applicationUserManager;
+
+        protected InternalActivityFilter(
+                                      IActivityContextQuery activityContextQuery,
+                                      IActivityContextCommand activityContextCommand,
+                                      ApplicationUserManager applicationUserManager)
+        {
+            _activityContextQuery = activityContextQuery;
+            _activityContextCommand = activityContextCommand;
+            _applicationUserManager = applicationUserManager;
+        }
+    }
+}
