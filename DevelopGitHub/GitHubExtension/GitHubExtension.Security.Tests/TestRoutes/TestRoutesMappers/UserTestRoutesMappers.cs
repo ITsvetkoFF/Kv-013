59195a0aa3abe427f94d0c7dd6fd62fe1534fe09
SerhiiStepanovych <hunterla2@gmail.com>
2016-05-13 18:28:23 +0300

    Add route tests

---
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Security.Tests/TestRoutes/TestRoutesMappers/UserTestRoutesMappers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using GitHubExtension.Infrastructure.Constants;
+
+namespace GitHubExtension.Security.Tests.TestRoutes.TestRoutesMappers
+{
+    public static class UserTestRoutesMappers
+    {
+        public static string ForGetAllUsersByName(this string url)
+        {
+            return url + RouteConstants.SearchUsersByName + "?username=test";
+        }
+    }
+}
---
+++ DevelopGitHub/GitHubExtension/GitHubExtension.Security.Tests/TestRoutes/UserTestRoutes.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using GitHubExtension.Infrastructure.Constants;
+using GitHubExtension.Security.Tests.TestRoutes.TestRoutesMappers;
+using GitHubExtension.Security.WebApi.Controllers;
+using MvcRouteTester;
+using Xunit;
+
+namespace GitHubExtension.Security.Tests.TestRoutes
+{
+    public class UserTestRoutes : TestRoutesConfig
+    {
+        public UserTestRoutes() : base(null)
+        {
+        }
+
+        [Fact]
+        public void GetAllUsersByNameRouteTest()
+        {
+            Url = Url.ForGetAllUsersByName();
+            Config.ShouldMap(Url).To<UserController>(HttpMethod.Get, x => x.GetAllUsersByName("test"));
+        }
+    }
+}
