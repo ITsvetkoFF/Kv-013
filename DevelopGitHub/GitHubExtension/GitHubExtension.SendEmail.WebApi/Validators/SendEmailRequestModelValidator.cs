bf4cd348e5b902b95fad532fdb9ea39a4892bc71
Rudeka <thotubox@gmail.com>
2016-05-18 17:07:21 +0300

    Add EmailRequest ViewModel

---
+++ DevelopGitHub/GitHubExtension/GitHubExtension.SendEmail.WebApi/Validators/SendEmailRequestModelValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+using GitHubExtension.SendEmail.WebApi.ViewModels;
+
+namespace GitHubExtension.SendEmail.WebApi.Validators
+{
+    public class SendEmailRequestModelValidator : AbstractValidator<SendEmailRequestModel>
+    {
+        public SendEmailRequestModelValidator()
+        {
+            RuleFor(m => m.ToEmail).NotEmpty();
+            RuleFor(m => m.ToEmail).EmailAddress();
+            RuleFor(m => m.Subject).NotEmpty();
+            RuleFor(m => m.Body).NotEmpty();
+        }
+    }
+}
